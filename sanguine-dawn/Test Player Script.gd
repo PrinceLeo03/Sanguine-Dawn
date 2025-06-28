16fcfd87ab17868a1ca22fd34fa8f507ab54623a
PrinceLeo <demothefox@gmail.com>
2025-06-28 15:54:01 +0100

    Player Movement Base

--- sanguine-dawn/Test Player Script.gd
+++ sanguine-dawn/Test Player Script.gd
@@ -1,18 +1,24 @@
 extends CharacterBody2D
 
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -600.0
+@export var holding_jump = false
+@export var fall_speed = 8
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
-
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	elif not is_on_floor():
+		velocity+= get_gravity() * fall_speed * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		print("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
